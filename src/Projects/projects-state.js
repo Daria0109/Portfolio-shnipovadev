85685aba38c217c028b4888839209c7056a0c13c
Daria0109 <da6ulya888@gmail.com>
2021-05-03 16:17:35 +0300

    Replace images

--- src/Projects/projects-state.js
+++ src/Projects/projects-state.js
@@ -1,14 +1,15 @@
-import todolistImg from './../assets/todolist-project.png';
-import quoteImg from './../assets/quote-project.png';
-import passwordImg from './../assets/password-project.png';
-import estateImg from './../assets/estate-project.png';
-import cardsImg from './../assets/cards-project.png'
+import project1 from './../assets/project1.png';
+import project2 from './../assets/project2.jpg';
+import project3 from './../assets/project3.jpg';
+import project4 from './../assets/project4.png';
+import project5 from './../assets/project5.png';
+import project6 from './../assets/project6.jpg';
 
 export const projects = [
   {
     title: "I'm Craving",
     description: 'STACK: React, TypeScript, SCSS, REST API, Axios, React-router-dom',
-    projectImg: cardsImg,
+    projectImg: project1,
     demoLink: 'https://imcraving.com',
     sourceCodeLink: 'https://github.com/Daria0109/Cards_ReactTS'
   },
@@ -16,35 +17,35 @@ export const projects = [
     title: 'Todolist',
     description: 'STACK: React, TypeScript, Redux, Redux Toolkit, Material UI, Formik,' +
       'REST API, Redux-thunk, Axios, React-router-dom',
-    projectImg: todolistImg,
+    projectImg: project2,
     demoLink: 'https://daria0109.github.io/Todolist_ReactTS',
     sourceCodeLink: 'https://github.com/Daria0109/Todolist_ReactTS'
   },
   {
     title: 'Quote of the day',
     description: 'STACK: JavaScript, CSS3, HTML5, Animate.css, Google Fonts, Font Awesome Icons',
-    projectImg: quoteImg,
+    projectImg: project3,
     demoLink: 'https://daria0109.github.io/QuotesGenerator_JS',
     sourceCodeLink: 'https://github.com/Daria0109/QuotesGenerator_JS'
   },
   {
     title: 'Smart cards',
     description: 'STACK: React, Redux, TypeScript, SCSS, REST API, Redux-thunk, Axios, React-router-dom',
-    projectImg: cardsImg,
+    projectImg: project4,
     demoLink: 'https://daria0109.github.io/Cards_ReactTS',
     sourceCodeLink: 'https://github.com/Daria0109/Cards_ReactTS'
   },
   {
     title: 'Password generator',
     description: 'STACK: JavaScript, CSS3, HTML5',
-    projectImg: passwordImg,
+    projectImg: project5,
     demoLink: 'https://daria0109.github.io/PasswordGenerator_JS',
     sourceCodeLink: 'https://github.com/Daria0109/PasswordGenerator_JS'
   },
   {
     title: 'Landing for estate agency',
     description: 'STACK: HTML5, SCSS, BEM',
-    projectImg: estateImg,
+    projectImg: project6,
     demoLink: 'https://daria0109.github.io/to2rent_html-css',
     sourceCodeLink: 'https://github.com/Daria0109/to2rent_html-css'
   },
